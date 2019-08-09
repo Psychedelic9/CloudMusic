d1e324d234300036586f2fb59630b6a761f3f704
Psychedelic9 <weeqqq@qq.com>
2019-08-09 18:34:40 +0800

    add

--- app/src/main/java/com/bai/psychedelic/cloudmusic/activity/MainActivity.java
+++ app/src/main/java/com/bai/psychedelic/cloudmusic/activity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.bai.psychedelic.cloudmusic.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.bai.psychedelic.cloudmusic.R;
+import com.bai.psychedelic.cloudmusic.adapter.MusicGridAdapter;
 import com.bai.psychedelic.cloudmusic.base.BaseActivity;
+import com.bai.psychedelic.cloudmusic.view.GridSpaceItemDecoration;
 
 public class MainActivity extends BaseActivity {
-
+    private RecyclerView mRvGrid;
+    private MusicGridAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +21,10 @@ public class MainActivity extends BaseActivity {
     }
     private void initView(){
         initNavBar(false,getString(R.string.cloud_music),true);
+        mRvGrid = findViewById(R.id.rv_grid);
+        mRvGrid.setLayoutManager(new GridLayoutManager(this,3));
+        mRvGrid.addItemDecoration(new GridSpaceItemDecoration(getResources().getDimensionPixelOffset(R.dimen.albumMarginSize),mRvGrid));
+        mAdapter = new MusicGridAdapter(this);
+        mRvGrid.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/bai/psychedelic/cloudmusic/view/WEqualsHImageView.java
@@ -0,0 +1,28 @@
+package com.bai.psychedelic.cloudmusic.view;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class WEqualsHImageView extends AppCompatImageView {
+    public WEqualsHImageView(Context context) {
+        super(context);
+    }
+
+    public WEqualsHImageView(Context context,  AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WEqualsHImageView(Context context,  AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+//        int width = MeasureSpec.getSize(widthMeasureSpec);
+//        int mode = MeasureSpec.getMode(widthMeasureSpec);
+    }
+}
